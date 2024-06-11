eb0ff17447ae9db95d75459e11c504f5f5b2f032
Douwe Schulte <d.schulte@uu.nl>
2024-06-11 13:18:54 +0200

    Fix some issues in consecutive alignment

--- rustyms/src/align/consecutive.rs
+++ rustyms/src/align/consecutive.rs
@@ -14,10 +14,10 @@ use itertools::Itertools;
 /// Align one sequence to multiple consecutive genes. Each gene can be controlled to be global to the left or free to allow unmatched residues between it and the previous gene.
 /// If the sequence is too short to cover all genes only the genes that could be matched are returned.
 /// # Panics
-/// If there are not two or more genes listed.
+/// If there are not two or more genes listed. If the return number is 0.
 #[cfg(feature = "imgt")]
-pub fn consecutive_align<const STEPS: u16>(
-    sequence: &LinearPeptide<Simple>,
+pub fn consecutive_align<const STEPS: u16, A: Into<Simple> + Clone + Eq>(
+    sequence: &LinearPeptide<A>,
     genes: &[(GeneType, AlignType)],
     species: Option<HashSet<Species>>,
     chains: Option<HashSet<ChainType>>,
@@ -25,22 +25,25 @@ pub fn consecutive_align<const STEPS: u16>(
     tolerance: Tolerance<Mass>,
     matrix: &[[i8; AminoAcid::TOTAL_NUMBER]; AminoAcid::TOTAL_NUMBER],
     return_number: usize,
-) -> Vec<Vec<(Allele<'static>, Alignment<'static, ExtremelySimple, Simple>)>> {
+) -> Vec<Vec<(Allele<'static>, Alignment<'static, ExtremelySimple, A>)>> {
     assert!(genes.len() >= 2);
-    let mut output: Vec<Vec<(Allele<'static>, Alignment<'static, ExtremelySimple, Simple>)>> =
+    assert!(return_number != 0);
+
+    let mut output: Vec<Vec<(Allele<'static>, Alignment<'static, ExtremelySimple, A>)>> =
         Vec::with_capacity(genes.len());
 
     let mut prev = 0;
-    for n in 0..genes.len() {
-        let (left_sequence, use_species) = if n == 0 {
-            (sequence.clone(), species.clone())
-        } else {
-            prev += output[n - 1][0].1.start_b() + output[n - 1][0].1.len_b();
-            let mut left_sequence: LinearPeptide<Simple> =
-                sequence.clone().sequence.into_iter().skip(prev).collect();
-            left_sequence.c_term.clone_from(&sequence.c_term);
-            (left_sequence, Some([output[n - 1][0].0.species].into()))
-        };
+    for gene in genes {
+        let (left_sequence, use_species) = output.last().and_then(|v| v.first()).map_or_else(
+            || (sequence.clone(), species.clone()),
+            |last| {
+                prev += last.1.start_b() + last.1.len_b();
+                (
+                    sequence.sub_peptide(prev + 1..),
+                    Some([last.0.species].into()),
+                )
+            },
+        );
 
         if left_sequence.is_empty() {
             break;
@@ -51,16 +54,16 @@ pub fn consecutive_align<const STEPS: u16>(
                 species: use_species,
                 chains: chains.clone(),
                 allele: allele.clone(),
-                genes: Some([genes[n].0].into()),
+                genes: Some([gene.0].into()),
             }
             .germlines()
             .map(|seq| {
-                let alignment = align::<STEPS, ExtremelySimple, Simple>(
+                let alignment = align::<STEPS, ExtremelySimple, A>(
                     seq.sequence,
                     &left_sequence,
                     matrix,
                     tolerance,
-                    genes[n].1,
+                    gene.1,
                 )
                 .to_owned();
                 (seq, alignment)
@@ -76,10 +79,10 @@ pub fn consecutive_align<const STEPS: u16>(
 /// Align one sequence to multiple consecutive genes. Each gene can be controlled to be global to the left or free to allow unmatched residues between it and the previous gene.
 /// If the sequence is too short to cover all genes only the genes that could be matched are returned.
 /// # Panics
-/// If there are not two or more genes listed.
+/// If there are not two or more genes listed. If the return number is 0.
 #[cfg(all(feature = "rayon", feature = "imgt"))]
-pub fn par_consecutive_align<const STEPS: u16>(
-    sequence: &LinearPeptide<Simple>,
+pub fn par_consecutive_align<const STEPS: u16, A: Into<Simple> + Clone + Eq + Send + Sync>(
+    sequence: &LinearPeptide<A>,
     genes: &[(GeneType, AlignType)],
     species: Option<HashSet<Species>>,
     chains: Option<HashSet<ChainType>>,
@@ -87,24 +90,27 @@ pub fn par_consecutive_align<const STEPS: u16>(
     tolerance: Tolerance<Mass>,
     matrix: &[[i8; AminoAcid::TOTAL_NUMBER]; AminoAcid::TOTAL_NUMBER],
     return_number: usize,
-) -> Vec<Vec<(Allele<'static>, Alignment<'static, ExtremelySimple, Simple>)>> {
+) -> Vec<Vec<(Allele<'static>, Alignment<'static, ExtremelySimple, A>)>> {
     use rayon::iter::ParallelIterator;
 
     assert!(genes.len() >= 2);
-    let mut output: Vec<Vec<(Allele<'static>, Alignment<'static, ExtremelySimple, Simple>)>> =
+    assert!(return_number != 0);
+
+    let mut output: Vec<Vec<(Allele<'static>, Alignment<'static, ExtremelySimple, A>)>> =
         Vec::with_capacity(genes.len());
 
     let mut prev = 0;
-    for n in 0..genes.len() {
-        let (left_sequence, use_species) = if n == 0 {
-            (sequence.clone(), species.clone())
-        } else {
-            prev += output[n - 1][0].1.start_b() + output[n - 1][0].1.len_b();
-            let mut left_sequence: LinearPeptide<Simple> =
-                sequence.clone().sequence.into_iter().skip(prev).collect();
-            left_sequence.c_term.clone_from(&sequence.c_term);
-            (left_sequence, Some([output[n - 1][0].0.species].into()))
-        };
+    for gene in genes {
+        let (left_sequence, use_species) = output.last().and_then(|v| v.first()).map_or_else(
+            || (sequence.clone(), species.clone()),
+            |last| {
+                prev += last.1.start_b() + last.1.len_b();
+                (
+                    sequence.sub_peptide(prev + 1..),
+                    Some([last.0.species].into()),
+                )
+            },
+        );
 
         if left_sequence.is_empty() {
             break;
@@ -115,16 +121,16 @@ pub fn par_consecutive_align<const STEPS: u16>(
                 species: use_species,
                 chains: chains.clone(),
                 allele: allele.clone(),
-                genes: Some([genes[n].0].into()),
+                genes: Some([gene.0].into()),
             }
             .par_germlines()
             .map(|seq| {
-                let alignment = align::<STEPS, ExtremelySimple, Simple>(
+                let alignment = align::<STEPS, ExtremelySimple, A>(
                     seq.sequence,
                     &left_sequence,
                     matrix,
                     tolerance,
-                    genes[n].1,
+                    gene.1,
                 );
                 (seq, alignment.to_owned())
             })
