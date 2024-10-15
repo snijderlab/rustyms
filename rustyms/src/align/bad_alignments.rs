f7498ca8b5d483942898f3f3d188fd96bb4e5482
Douwe Schulte <d.schulte@uu.nl>
2024-10-15 13:39:59 +0200

    Added more bad alignment examples

--- rustyms/src/align/bad_alignments.rs
+++ rustyms/src/align/bad_alignments.rs
@@ -10,6 +10,16 @@ fn overextended_rotation() {
     test_alignment("IVQEVS", "LEVQVES", "1i1I2=2r1=");
 }
 
+#[test]
+fn no_detected_rotation() {
+    test_alignment("AGGHT", "ANTH", "1=2:1i2r");
+}
+
+#[test]
+fn no_detected_rotation_2() {
+    test_alignment("AGGHTK", "ANTHK", "1=2:1i2r1=");
+}
+
 fn test_alignment(peptide_one: &str, peptide_two: &str, path: &str) {
     let first_peptide = LinearPeptide::pro_forma(peptide_one, None)
         .unwrap()
