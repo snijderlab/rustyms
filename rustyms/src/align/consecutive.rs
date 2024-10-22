0a6f30dd925a201a5be04273ecc337e24ef8061a
Douwe Schulte <d.schulte@uu.nl>
2024-10-22 15:01:53 +0200

    Added consecutive alignment structure #30

--- rustyms/src/align/consecutive.rs
+++ rustyms/src/align/consecutive.rs
@@ -9,12 +9,77 @@ use std::collections::HashSet;
 
 use itertools::Itertools;
 
-/// Only available with if features `align` and `imgt` are turned on.
+/// A consecutive alignment, which align one sequence to multiple sequences.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct ConsecutiveAlignment<'lifetime, A> {
+    /// All underlying alignments, per gene there is a vector containing all options for that gene.
+    pub alignments: Vec<Vec<(Allele<'lifetime>, Alignment<'lifetime, UnAmbiguous, A>)>>,
+}
+
+impl<'lifetime, A> ConsecutiveAlignment<'lifetime, A> {
+    /// Get the main alignment, the alignment taking the best alignment for each gene.
+    pub fn main_alignment(
+        &self,
+    ) -> Vec<&(Allele<'lifetime>, Alignment<'lifetime, UnAmbiguous, A>)> {
+        self.alignments.iter().filter_map(|a| a.first()).collect()
+    }
+}
+
+impl<'lifetime, A: AtMax<Linear>> ConsecutiveAlignment<'lifetime, A> {
+    /// Break up in the main alignment into the regions as annotated in the alleles.
+    #[allow(clippy::missing_panics_doc)]
+    pub fn regions(&self) -> Vec<(LinearPeptide<A>, Region)> {
+        let mut b_offset = 0;
+        self.alignments
+            .iter()
+            .filter_map(|a| {
+                a.first().map(|(allele, alignment)| {
+                    let mut index_a = alignment.start_a;
+                    let mut start_region_b = alignment.start_b;
+                    let mut index_b = alignment.start_b;
+                    let mut region = allele.region(index_a).map_or(Region::FR1, |(r, _)| r);
+                    let mut ranges = Vec::new();
+
+                    for step in &alignment.path {
+                        let new_region = allele
+                            .region(index_a + step.step_a as usize)
+                            .map_or(Region::FR1, |(r, _)| r);
+                        if region != new_region {
+                            ranges.push((b_offset + start_region_b, b_offset + index_b, region));
+                            start_region_b = index_b + 1;
+                            region = new_region;
+                        }
+                        index_a += step.step_a as usize;
+                        index_b += step.step_b as usize;
+                    }
+                    b_offset += alignment.len_b() + alignment.start_b;
+
+                    ranges
+                })
+            })
+            .flatten()
+            .chunk_by(|(_, _, r)| *r)
+            .into_iter()
+            .map(|(region, mut chunk)| {
+                let first = chunk.next().unwrap();
+                let last = chunk.last().unwrap_or(first);
+                (
+                    self.alignments[0][0]
+                        .1
+                        .seq_b()
+                        .sub_peptide(first.0..=last.1),
+                    region,
+                )
+            })
+            .collect()
+    }
+}
+
+/// Only available if features `align` and `imgt` are turned on.
 /// Align one sequence to multiple consecutive genes. Each gene can be controlled to be global to the left or free to allow unmatched residues between it and the previous gene.
 /// If the sequence is too short to cover all genes only the genes that could be matched are returned.
 /// # Panics
 /// If there are not two or more genes listed. If the return number is 0.
-#[cfg(feature = "imgt")]
 #[allow(clippy::needless_pass_by_value)]
 pub fn consecutive_align<const STEPS: u16, A: AtMax<SimpleLinear> + AtMax<Linear>>(
     sequence: &LinearPeptide<A>,
@@ -24,7 +89,7 @@ pub fn consecutive_align<const STEPS: u16, A: AtMax<SimpleLinear> + AtMax<Linear
     allele: AlleleSelection,
     scoring: AlignScoring<'_>,
     return_number: usize,
-) -> Vec<Vec<(Allele<'static>, Alignment<'static, UnAmbiguous, A>)>> {
+) -> ConsecutiveAlignment<'static, A> {
     assert!(genes.len() >= 2);
     assert!(return_number != 0);
 
@@ -68,7 +133,7 @@ pub fn consecutive_align<const STEPS: u16, A: AtMax<SimpleLinear> + AtMax<Linear
             .collect_vec(),
         );
     }
-    output
+    ConsecutiveAlignment { alignments: output }
 }
 
 /// Only available with if features `align`, `rayon`, and `imgt` are turned on.
@@ -76,7 +141,7 @@ pub fn consecutive_align<const STEPS: u16, A: AtMax<SimpleLinear> + AtMax<Linear
 /// If the sequence is too short to cover all genes only the genes that could be matched are returned.
 /// # Panics
 /// If there are not two or more genes listed. If the return number is 0.
-#[cfg(all(feature = "rayon", feature = "imgt"))]
+#[cfg(all(feature = "rayon"))]
 #[allow(clippy::needless_pass_by_value)]
 pub fn par_consecutive_align<
     const STEPS: u16,
@@ -89,7 +154,7 @@ pub fn par_consecutive_align<
     allele: AlleleSelection,
     scoring: AlignScoring<'_>,
     return_number: usize,
-) -> Vec<Vec<(Allele<'static>, Alignment<'static, UnAmbiguous, A>)>> {
+) -> ConsecutiveAlignment<'static, A> {
     use rayon::iter::ParallelIterator;
 
     assert!(genes.len() >= 2);
@@ -136,5 +201,5 @@ pub fn par_consecutive_align<
             .collect_vec(),
         );
     }
-    output
+    ConsecutiveAlignment { alignments: output }
 }
